eb7a266e49b9eb977067147594967ac9ec250dfc
yvsc <108088938+yvsc@users.noreply.github.com>
2025-12-07 10:23:43 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 // Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
 // or a Google Sheets URL (File > Publishconst dataLocatio to the Web > CSV )
-const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTKNw37qP9-YP7LWivnBwxE0zmeLDxh-RAzH4ZpFkoasFsKofQzttDEpT-OYb51o2eL7Vl9aewaPHqj/pub?gid=0&single=true&output=csv';
+const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9WLUZxOTk43gpOfJUlir_8xUSGWSZ0D9Tav-f_H2GpaA6fb4rYpQ5_obzqoBInwE4Pv9FmbDrKtfb/pub?gid=70869349&single=true&output=csv';
 // Map's initial center, and zoom level
 const mapCenter = [50.511, -107.290]; 
 const mapZoom = 9;
